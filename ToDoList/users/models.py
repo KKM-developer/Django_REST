c15905fe300aae6ac46c52bbd7269e2d5314baa8
KKM-dev <cirill@mail.ru>
2021-05-30 18:19:10 +0300

    не отображаются юзеры

--- ToDoList/users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    avatar = models.ImageField(upload_to='users_avatar', blank=True)
